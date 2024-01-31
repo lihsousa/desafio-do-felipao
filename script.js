db593ae357955a7d9a1cdb3d51c4d1caf64f2ea6
lihsousa <lidianosousa@gmail.com>
2024-01-31 15:07:25 -0300

    Desafio do Felipão

---
+++ script.js
@@ -0,0 +1,44 @@
+ 
+let nome = "Lidiano"
+let xp = 7010
+let nivel = ""
+
+if(xp < 1000){
+  nivel = " Ferro"
+ferro = ` O herói de nome ${nome} está no nível de ${nivel}`;
+console.log(ferro)
+}else if(xp <=2000){
+    nivel = " Bronze"
+    bronze = ` O herói de nome ${nome} está no nível de ${nivel}`;
+    console.log(bronze)
+}else if(xp <=5000){
+    nivel = " Prata"
+    prata = ` O herói de nome ${nome} está no nível de ${nivel}`;
+    console.log(prata)
+}else if(xp <=7000){
+    nivel = "Ouro"
+    ouro = ` O herói de nome ${nome} está no nível de ${nivel}`;
+    console.log(ouro)
+}else if(xp <=8000){
+    nivel = " Pratina"
+    pratina = ` O herói de nome ${nome} está no nível de ${nivel}`;
+    console.log(pratina)
+}else if(xp <=9000){
+    nivel = "Ascendente"
+    ascendente = ` O herói de nome ${nome} está no nível de ${nivel}`;
+    console.log(ascendente)
+}else if(xp <=10000){
+    nivel = "Imortal"
+    imortal = ` O herói de nome ${nome} está no nível de ${nivel}`;
+    console.log(imortal)
+}else{
+    nivel = "Radiante"
+    radiante = ` O herói de nome ${nome} está no nível de ${nivel}`;
+    console.log(radiante)
+}
+
+
+
+
+
+
